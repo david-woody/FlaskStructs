135f6455eae2999a7d1c555e9e7b0418efc49ac4
Wallent <ding.woody@gmail.com>
2017-05-22 16:13:15 +0800

    init

---
+++ app/main/errors.py
@@ -0,0 +1,3 @@
+from flask import render_template
+
+from app.main import main
---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+BaseDir = os.path.abspath(os.path.dirname(__file__))
+DbDir = os.path.join(BaseDir, 'db/')
+
+
+class Config:
+    SECURITY_KEY = os.environ.get('SECRET_KEY') or "'I dont know what's that!"
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    pass
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class TestingConfig(Config):
+    pass
+
+
+config = {
+    'develop': DevelopmentConfig,
+    'test': TestingConfig,
+    'product': ProductionConfig,
+    'default': DevelopmentConfig
+}
